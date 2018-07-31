c9b92d8781aab8c06cced5a29a2c79a8cd621c55
samoye <wu.yangjie@foxmail.com>
2018-07-31 09:25:17 +0800

    add tests model

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,10 +11,10 @@ def create_app(test_config = None):
 )
 
     if test_config is None:
-    # load the instance config, if it exists,when not testing
-        app.config.from_pyfile('config.py',silent=True)
+        # load the instance config, if it exists,when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
-    # load the test config if passed in
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
@@ -23,7 +23,7 @@ def create_app(test_config = None):
     except OSError:
         pass
 
-    # a simpla page tha says hello
+    # a simple page tha says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World'
